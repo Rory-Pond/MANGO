1679695fd41903a1c0b143b43dcbab68f72e8852
Rory-Pond <rorypnd@gmail.com>
2015-10-22 15:22:28 +0100

    Initialising Project
    Creating Visual Studio Solution and adding Libraries (SDL, glew, glm)

--- README.md
+++
@@ -1,2 +0,0 @@
-# MANGO
-A 3D visualization tool for richard-evans' atomistic simulator for magnetic materials "VAMPIRE"
---
+++ Source/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
+
