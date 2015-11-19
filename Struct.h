39f245cd01aee7667a6073cb92d9d5bce4e4a107
Rory-Pond <rorypnd@gmail.com>
2015-11-19 11:56:33 +0000

    Data Input Class
    Class with methods for inputing data from VAPMIRE files

---
+++ Struct.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include <glm/glm.hpp>
+
+struct systemInfo {
+	int numAtoms;
+	int numMaterials;
+	std::vector <double> Material;
+	glm::vec3 dimensions;
+};
+
+struct spinInfo {
+	int StepNum;
+	double Time;
+	double Field;
+	double Tempreture;
+	glm::vec3 Magnetisation;
+};
